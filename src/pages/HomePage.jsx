18dccd639936597217f1ad38cd03a579be87a7de
Alkaou Dit Baba Dembele <alkaouditbabad@gmail.com>
2025-01-23 22:12:06 +0000

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen max-h-full">
+      <div className="font-bold text-gray">Hello app</div>
+    </div>
+  );
+}
