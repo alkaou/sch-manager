bd48b06430aa926d72439db1a826cc9b8bca94ef
Alkaou Dit Baba Dembele <alkaouditbabad@gmail.com>
2025-04-11 17:47:33 +0000

    Added authentification users with google account and firebase système.

--- test/proxy.test.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const apiProxy = createProxyMiddleware({
-  target: 'http://www.example.org',
-  changeOrigin: true,
-});
-
-// 'apiProxy' is now ready to be used as middleware in a server.
-console.log(apiProxy);
